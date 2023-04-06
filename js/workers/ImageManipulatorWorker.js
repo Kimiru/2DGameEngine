a299a254ae539f2af6f187a97d126f5653f6da97
Kimiru <laursim24@gmail.com>
2023-04-06 09:49:00 +0200

    forgot

--- js/workers/ImageManipulatorWorker.js
+++
@@ -1,7 +0,0 @@
-onmessage = async (event) => {
-    let canvas = event.data;
-    let ctx = canvas.getContext('2d');
-    let blob = await canvas.convertToBlob();
-    let blobUrl = URL.createObjectURL(blob);
-    postMessage(blobUrl);
-};
